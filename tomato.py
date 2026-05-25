b32a3235d47a35d54ba3a536a1ba3a18746dc4be
202611230-boop <2026_11230@kshcm.net>
2026-05-25 13:57:55 +0700

    Update tomato.py

--- tomato.py
+++ tomato.py
@@ -1,13 +1,29 @@
-temp = float(input("내부온도 입력 : "))
-humidity = float(input("내부습도 입력 : "))
-soil_temp = float(input("지온 입력 : "))
+import streamlit as st
+import pandas as pd
+import joblib
 
+# 모델 로드
+rf_model = joblib.load("tomato_model.pkl")
 
-# DataFrame으로 변환(2차원 배열 형태로 입력)
-input_data = pd.DataFrame([[temp, humidity, soil_temp]], columns=['내부온도', '내부습도', '지온'])
+# 제목
+st.title("착과율 예측 프로그램")
 
+# 입력
+temp = st.number_input("내부온도 입력", value=25.0)
+humidity = st.number_input("내부습도 입력", value=60.0)
+soil_temp = st.number_input("지온 입력", value=20.0)
 
-# 예측
-predicted = rf_model.predict(input_data)
+# 버튼
+if st.button("예측하기"):
 
-print(f"예측 착과율 : {predicted[0]:.1f}%")
+    # 입력 데이터를 DataFrame으로 변환
+    input_data = pd.DataFrame(
+        [[temp, humidity, soil_temp]],
+        columns=['내부온도', '내부습도', '지온']
+    )
+
+    # 예측
+    predicted = rf_model.predict(input_data)
+
+    # 결과 출력
+    st.write(f"### 예측 착과율 : {predicted[0]:.1f}%")
